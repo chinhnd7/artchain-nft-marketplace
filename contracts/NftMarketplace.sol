d399ec69c2bfd6fd9094149462cac08d3af00147
chinhnd7 <ndchinh110797@gmail.com>
2023-05-18 18:19:36 +0700

    listItem and buyItem function

---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+error NftMarketplace__PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotOwner();
+error NftMarketplace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
+
+contract NftMarketplace {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    event ItemBought(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // NFT Contract address -> NFT TokenID -> Listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    // Seller address -> Amount earned
+    mapping(address => uint256) private s_proceeds; // proceeds (Tiền thu được)
+
+    ////////////////////
+    //    Modifiers   //
+    ////////////////////
+    // Function Modifiers are used to modify the behaviour of a function
+    // It can be used to​​ validate the owner or validate the value of an action.
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+
+        // check modifier first, run command in that function after
+        if (listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+        // If "_" is before "revert" command, it runs commands in that function first.
+    }
+
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price <= 0) {
+            revert NftMarketplace__NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    ////////////////////
+    // Main Functions //
+    ////////////////////
+
+    /*
+     * @notice Method for listing your NFT on the marketplace
+     * @param nftAddress: Address of the NFT
+     * @param tokenId: The Token ID of the NFT
+     * @param price: sale price of the listed NFT
+     * @dev Technically, we could have contract be the escrow for the NFTs
+     * but this way people can still hold their NFTs when listed.
+     */
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        // Challenge: Have this contract accept payment in a subset of tokens as well
+        // Hint: Use Chainlink Price Feeds to convert the price of the tokens between each others
+        notListed(nftAddress, tokenId, msg.sender)
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
+        if (price <= 0) {
+            revert NftMarketplace__PriceMustBeAboveZero();
+        }
+        // 1. Send the NFT to the contract. Transfer -> Contract "hold" the NFT.
+        // 2. Owners can still hold their NFT, and give the marketplace approval
+        // to sell the NFT for them.
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketplace__NotApprovedForMarketplace();
+        }
+
+        // array? mapping?
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    ) external payable isListed(nftAddress, tokenId) {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        if (msg.value < listedItem.price) {
+            revert NftMarketplace__PriceNotMet(nftAddress, tokenId, listedItem.price);
+        }
+
+        // We don't just send the seller the money...?
+        // https://fravoll.github.io/solidity-patterns/pull_over_push.html
+
+        // Sending the money to the user    "N"
+        // => Have them withdraw the money  "Y"
+        s_proceeds[listedItem.seller] = s_proceeds[listedItem.seller] + msg.value;
+        // once user buy this item, delete the listing of item
+        delete (s_listings[nftAddress][tokenId]);
+        // transfer
+        IERC721(nftAddress).safeTransferFrom(listedItem.seller, msg.sender, tokenId);
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
+}
+
+// 1. `listItem`: List NFTs on the marketplace
+// 2. `buyItem`: Buy the NFTs
+// 3. `cancelItem`: Cancel a listing
+// 4. `updateListing`: Update Price
+// 5. `withdrawProceeds`: Withdraw payment for my bought NFTs
