8e71bd086cb35f95cce4346dae88db2759a9a7df
chinhnd7 <ndchinh110797@gmail.com>
2023-05-24 15:10:58 +0700

    first unit test n fix an error in function connect

--- deploy/01-deploy-nft-marketplace.js
+++ deploy/01-deploy-nft-marketplace.js
@@ -1,14 +1,10 @@
-const { network, ethers } = require("hardhat")
-const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
-
-    if (developmentChains.includes(network.name)) {
-    }
 
     log("------------------------------------")
     const arguments = []
@@ -18,12 +14,12 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
-    log("------------------------------------")
 
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
         await verify(nftMarketplace.address, arguments)
     }
+    log("------------------------------------")
 }
 
 module.exports.tags = ["all", "nftmarketplace"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,8 @@ require("hardhat-contract-sizer")
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const SEPOLIA_RPC_URL =
     process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "https://polygon-mumbai.g.alchemy.com/v2/YOUR-API-KEY"
+const MUMBAI_RPC_URL =
+    process.env.MUMBAI_RPC_URL || "https://polygon-mumbai.g.alchemy.com/v2/YOUR-API-KEY"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const MUMBAI_API_KEY = process.env.MUMBAI_API_KEY || ""
@@ -69,6 +70,9 @@ module.exports = {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
             1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        user: {
+            default: 1,
         },
     },
     mocha: {
